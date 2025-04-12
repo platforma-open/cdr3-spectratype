619231e5a51c636783d38d44fe7f32200b9210c6
Stanislav Poslavsky <poslavskysv@milaboratories.com>
2025-04-12 17:34:43 +0200

    Block v2

--- ui/src/app.ts
+++ ui/src/app.ts
@@ -1,15 +1,15 @@
 import { model } from '@platforma-open/milaboratories.cdr3-spectratype.model';
 import { defineApp } from '@platforma-sdk/ui-vue';
 import BubblePlot from './pages/BubblePlot.vue';
-import MainPage from './pages/MainPage.vue';
-import StackedBarPlot from './pages/StackedBarPlot.vue';
+import CDR3StackedBarPlot from './pages/CDR3StackedBarPlot.vue';
+import VStackedBarPlot from './pages/VStackedBarPlot.vue';
 
 export const sdkPlugin = defineApp(model, () => {
   return {
     routes: {
-      '/': () => MainPage,
-      '/bubblePlot': () => BubblePlot,
-      '/stackedBarPlot': () => StackedBarPlot,
+      '/': () => BubblePlot,
+      '/vStackedBarPlot': () => VStackedBarPlot,
+      '/cdr3StackedBarPlot': () => CDR3StackedBarPlot,
     },
   };
 });
--- ui/src/pages/BubblePlot.vue
+++ ui/src/pages/BubblePlot.vue
@@ -2,60 +2,108 @@
 import type { GraphMakerProps } from '@milaboratories/graph-maker';
 import { GraphMaker } from '@milaboratories/graph-maker';
 import '@milaboratories/graph-maker/styles';
-// import { ref } from 'vue';
+import type { PDataColumnSpec } from '@platforma-sdk/model';
+import { PlBtnGroup, PlDropdown, PlDropdownRef } from '@platforma-sdk/ui-vue';
+import { computed, useTemplateRef } from 'vue';
 import { useApp } from '../app';
 
 const app = useApp();
 
-const defaultOptions: GraphMakerProps['defaultOptions'] = [
-  {
-    inputName: 'valueSize',
-    selectedSource: {
-      kind: 'PColumn',
-      valueType: 'Float',
-      name: 'pl7.app/vdj/cdr3Spectratype',
-      axesSpec: [],
+const defaultOptions = computed((): GraphMakerProps['defaultOptions'] => {
+  const mainCol: PDataColumnSpec = {
+    kind: 'PColumn',
+    valueType: 'Float',
+    name: 'pl7.app/vdj/vSpectratype',
+    domain: {
+      'pl7.app/vdj/cdr3Spectratype/type': app.model.ui.weightedFlag ? 'weighted' : 'unweighted',
     },
+    axesSpec: [],
+  };
+  return [
+    {
+      inputName: 'valueSize',
+      selectedSource: mainCol,
+    },
+    {
+      inputName: 'valueColor',
+      selectedSource: mainCol,
+    },
+    {
+      inputName: 'x',
+      selectedSource: {
+        type: 'String',
+        name: 'pl7.app/vdj/geneHit',
+      },
+    },
+    {
+      inputName: 'y',
+      selectedSource: {
+        type: 'Int',
+        name: 'pl7.app/vdj/cdr3Length',
+      },
+    },
+    {
+      inputName: 'tabBy',
+      selectedSource: {
+        type: 'String',
+        name: 'pl7.app/sampleId',
+      },
+    },
+  ];
+});
+
+const weightOptions = [
+  {
+    label: 'Weighted',
+    value: true,
   },
   {
-    inputName: 'valueColor',
-    selectedSource: {
-      kind: 'PColumn',
-      valueType: 'Float',
-      name: 'pl7.app/vdj/cdr3Spectratype',
-      axesSpec: [],
-    },
-  },
-  {
-    inputName: 'x',
-    selectedSource: {
-      type: 'String',
-      name: 'pl7.app/vdj/geneHit',
-    },
-  },
-  {
-    inputName: 'y',
-    selectedSource: {
-      type: 'Int',
-      name: 'pl7.app/vdj/cdr3Length',
-    },
-  },
-  {
-    inputName: 'tabBy',
-    selectedSource: {
-      type: 'String',
-      name: 'pl7.app/sampleId',
-    },
+    label: 'Unweighted',
+    value: false,
   },
 ];
 
+const graphMakerRef = useTemplateRef('graphMaker');
+
+const setWeightedFlag = (flag: boolean) => {
+  app.model.ui.weightedFlag = flag;
+  graphMakerRef.value?.reset();
+};
 </script>
 
 <template>
   <GraphMaker
+    ref="graphMaker"
     v-model="app.model.ui.bubblePlotState"
     chart-type="bubble"
     :p-frame="app.model.outputs.pf"
     :default-options="defaultOptions"
-  />
+    :readonly-inputs="['x', 'y', 'valueSize', 'valueColor']"
+  >
+    <template #titleLineSlot>
+      <PlBtnGroup v-model="app.model.ui.weightedFlag" :options="weightOptions" @v-model:set="setWeightedFlag"/>
+    </template>
+    <template #settingsSlot>
+      <PlDropdownRef
+        v-model="app.model.args.cdr3Ref"
+        :options="app.model.outputs.cdr3Options"
+        label="CDR3 sequence"
+        required
+      />
+
+      <PlDropdown
+        v-model="app.model.args.abundanceRef"
+        :options="app.model.outputs.abundanceOptions"
+        label="Abundance (weight)"
+        required
+      />
+
+      <PlDropdown
+        v-model="app.model.args.vGeneRef"
+        :options="app.model.outputs.vGeneOptions"
+        label="V gene"
+        required
+      />
+    </template>
+  </GraphMaker>
 </template>
