1c9732abbc55ac892f756fe40c6904504ea4a312
Mariia Zueva <maryzueva@gmail.com>
2026-01-20 13:13:51 +0100

    Labels migration

--- ui/src/app.ts
+++ ui/src/app.ts
@@ -1,5 +1,6 @@
 import { model } from '@platforma-open/milaboratories.cdr3-spectratype.model';
 import { defineApp } from '@platforma-sdk/ui-vue';
+import { watch } from 'vue';
 import BubblePlot from './pages/BubblePlot.vue';
 import CDR3StackedBarPlot from './pages/CDR3StackedBarPlot.vue';
 import VStackedBarPlot from './pages/VStackedBarPlot.vue';
@@ -15,3 +16,12 @@ export const sdkPlugin = defineApp(model, () => {
 });
 
 export const useApp = sdkPlugin.useApp;
+
+// Make sure labels are initialized
+const unwatch = watch(sdkPlugin, ({ loaded }) => {
+  if (!loaded) return;
+  const app = useApp();
+  app.model.args.customBlockLabel ??= '';
+  app.model.args.defaultBlockLabel ??= 'Select Dataset';
+  unwatch();
+});
