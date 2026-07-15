1c1ebe081a0393c00d21195d8e298f143fcc6b60
Julen Mendieta <jmendieta@milaboratories.com>
2026-07-15 12:09:19 +0200

    Prevent Light chain in single-cell VHH input

--- ui/src/pages/Settings.vue
+++ ui/src/pages/Settings.vue
@@ -7,7 +7,10 @@ import { lengthTypeOptions, useIsSingleCell, useScChainOptions } from "../utils"
 const app = useApp();
 
 const isSingleCell = useIsSingleCell(computed(() => app.model.outputs.datasetSpec));
-const scChainOptions = useScChainOptions(computed(() => app.model.outputs.datasetSpec));
+const scChainOptions = useScChainOptions(
+  computed(() => app.model.outputs.datasetSpec),
+  computed(() => app.model.outputs.availableScChains),
+);
 </script>
 
 <template>
@@ -33,7 +36,7 @@ const scChainOptions = useScChainOptions(computed(() => app.model.outputs.datase
   />
 
   <PlBtnGroup
-    v-if="isSingleCell"
+    v-if="isSingleCell && (scChainOptions?.length ?? 0) > 1"
     v-model="app.model.data.scChain"
     :options="scChainOptions ?? []"
     label="Chain"
